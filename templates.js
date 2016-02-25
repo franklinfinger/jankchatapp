6d62c2413b5c5a85cabf5d5c252f6264892c9927
frank finger <franklinfinger@franks-MacBook-Pro.local>
2016-02-25 14:40:26 -0500

    add enterChat template

---
+++ templates.js
@@ -0,0 +1,12 @@
+var templates {
+  enterChat: [
+    '<div class="enter-msg-container">',
+      '<div class="enter-msg-wrapper">',
+        '<p class="enter-msg">',
+          '<%= message %>',
+        '</p>',
+      '</div>',
+    '</div>',
+  '</div>'
+].join("")
+}
