e29560d5708675e74967770db1dc5fec277bda83
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-02-25 14:10:21 -0500

    started working on object literals

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-// this is main javascript //
+$(document).ready(function() {
+  myChat.init();
+});
+
+// myChat main Object calling initStyling & initEvents
+var myChat = {
+  init: function() {
+    myChat.initStyling();
+    myChat.initEvents();
+  },
+
+
+  // initStyling
+  initStyling: function() {
+    myChat.addAllPosts();
+  }, // end of initStyling
+
+  // initEvents
+  initEvents: function() {
+
+  getChats: function() {
+    return chats;
+  },
+  addChats: function(newChat) {
+
+  }
+  } // end of initEvents
+
+} // end of myChat
