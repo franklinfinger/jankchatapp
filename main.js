a3a8695a3f6614b8ff58a87d09e80fa680114ecc
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-02-25 21:49:36 -0500

    added frank functions to main

--- main.js
+++ main.js
@@ -13,54 +13,64 @@ var myChat = {
 
   // initStyling
   initStyling: function() {
-    myChat.addAllChatsToDom();
-    myChat.getChats();
+    // myChat.addAllChatsToDom();
+    // myChat.getChats();
   },
 
   // initEvents
   initEvents: function() {
     $('form').on('submit', myChat.submitChat);
+    $('section').on('click', '.delete', myChat.deleteChatFromDom);
   },
 
-submitChat: function(event) {
-  event.preventDefault();
-  var newChat = myChat.getChatFromDom();
-  console.log(newChat);
-  myChat.addChats(newChat);
-  $('input').val('');
-},
+  submitChat: function(event) {
+    event.preventDefault();
+    var newChat = myChat.getChatFromDom();
+    console.log(newChat);
+    myChat.addChats(newChat);
+    $('input').val('');
+  },
 
-getChatFromDom: function() {
-  var username = $('input[name="login"]').val();
-  var content = $('.enter-msg').val();
-  return {
-    username: username,
-    message: message
-  }
-},
-
-addAllChatsToDom: function(chatsArr) {
-  $('.post-msg-container').html('');
-  _.each(chatsArr, function(el) {
-    var tmpl = _.templates(templates.postChat);
-    $('.post-msg-container').append(tmpl(el));
-  });
-},
-
-getChats: function() {
-  $.ajax({
-    url: myChat.url,
-    method: 'GET',
-    success: function(chats) {
-      console.log(chats);
-      myChat.addAllChatsToDom(chats);
-    },
-    error: function(err) {
-      console.log(err);
+  getChatFromDom: function() {
+    // var username = $('input[name="login"]').val();
+    var content = $('.enter-msg').val();
+    console.log(content);
+    return {
+      // username: username,
+      message: content
     }
-  });
-
   },
+
+  addAllChatsToDom: function(chatsArr) {
+    // chatsArr.preventDefault();
+    $('.post-msg-container').html('');
+    _.each(chatsArr, function(el) {
+      var tmpl = _.templates(templates.postChat);
+      $('.post-msg-container').append(tmpl(el));
+    });
+  },
+
+  //delete event
+  deleteChatFromDom: function (event) {
+      var chatId = $(this).closest("post-msg-container").data('chatid');
+      myChat.deleteChats(chatId);
+      myChat.addAllChatsToDom();
+  },
+
+  getChats: function() {
+    $.ajax({
+      url: myChat.url,
+      method: 'GET',
+      success: function(chats) {
+        console.log(chats);
+        myChat.addAllChatsToDom(chats);
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });
+  },
+
   addChats: function(newChat) {
     $.ajax({
       url: myChat.url,
@@ -74,6 +84,19 @@ getChats: function() {
       }
     });
   },
+
+  deleteChats: function deleteChats(postId) {
+      console.log('http://tiny-tiny.herokuapp.com/collections/<collectionName>' + '/' + chatId);
+      $.ajax({
+        url: myChat.url + '/' + chatId,
+        method: "DELETE",
+        success: function (response) {
+          myChat.getChats();
+        }
+      });
+    },
+
+
 
 
 
