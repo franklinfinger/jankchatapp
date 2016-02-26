b631d711ccb50e9a51bf42182f2b62297e2ff9f3
Justin Lock <Home@Justins-MacBook-Pro.local>
2016-02-25 21:50:27 -0500

    saved frank file

--- frank.js
+++ frank.js
@@ -8,29 +8,13 @@ deleteChatFromDom: function (event) {
 
 
 
-deleteChats: function deleteChats(postId) {
-    console.log('http://tiny-tiny.herokuapp.com/collections/<collectionName>' + '/' + chatId);
-    $.ajax({
-      url: myChat.url + '/' + chatId,
-      method: "DELETE",
-      success: function (response) {
-        myChat.getChats();
-      }
-    });
-  },
 
-//delete event
-deleteChatFromDom: function (event) {
-    var chatId = $(this).closest("post-msg-container").data('chatid');
-    myChat.deleteChats(chatId);
-    myChat.addAllChatsToDom();
-  },
 
 
 
 //place this in the initEvents: function()
 
-$('section').on('click', '.delete', myBlog.deleteChatFromDom);
+
 
 
 
