070bdf0209b71c1248d3e0f31348263c503547f6
frank finger <franklinfinger@franks-MacBook-Pro.local>
2016-02-26 09:18:10 -0500

    trying to add username in frank.js

--- frank.js
+++ frank.js
@@ -19,6 +19,30 @@ deleteChatFromDom: function (event) {
 
 
 
-*** USER name *****
-var alert = prompt("Enter User Name")
-sessionStorage.setItem('key', 'value');
+
+
+
+
+
+
+<section>
+  <div id="user-login-data"></div>
+  <input type="text" id="login-name" />
+  <input type="button" id="submitbtn" value="Save Text to Local Storage" />
+</section>
+
+$("#submitbtn").click(function () {
+    SaveToLocalStorage();
+});
+
+function SaveToLocalStorage(){
+  var username = $("#login-name").val();
+  localStorage.setItem('LocalStorageKey', username);
+  RetrieveFromLocalStorage();
+}
+
+function RetrieveFromLocalStorage() {
+  var retrivedValue = 'None';
+  var retrivedValue = localStorage.getItem('LocalStorageKey', retrivedValue);
+  $("#user-login-data").text(retrivedValue);
+}
